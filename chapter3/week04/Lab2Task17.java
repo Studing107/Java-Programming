0a04a11fd1192f738b23380160088c5241eb04e7
Studing107 <mchappygame@163.com>
2024-09-30 10:21:22 +0800

    Create Lab2Task17.java

---
+++ chapter3/week04/Lab2Task17.java
@@ -0,0 +1,13 @@
+package com.zjnu.demo;
+
+import java.util.Scanner;
+
+public class Lab2Task17 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your lottery pick (two digits): ");
+        int lotteryPick = input.nextInt();
+        String formattedPick = String.format("%02d", lotteryPick);
+        System.out.println("Your formatted lottery pick is: " + formattedPick);
+    }
+}
