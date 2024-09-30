ec3e91a624d054b726f07aa538296b5feeb564e9
Studing107 <mchappygame@163.com>
2024-09-30 09:26:03 +0800

    Update task05.java

--- chapter3/week03/task05.java
+++ chapter3/week03/task05.java
@@ -2,17 +2,18 @@ import java.util.Scanner;
 
 public class task05 {
     public static void main(String[] args) {
-        double w, h;
+         public static void main(String[] args) {
+        double weight, height;
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the weight in pounds: ");
-        w = sc.nextDouble();
+        weight = sc.nextDouble();
         System.out.print("Enter the height in inches: ");
-        h = sc.nextDouble();
+        height = sc.nextDouble();
 
-        double Wkilo = w * 0.45359237;
-        double Hmet = h * 0.0254;
+        double weightInKilograms = weight * 0.45359237;
+        double heightInMeters = height * 0.0254;
 
-        double bmi = Wkilo / (Hmet * Hmet);
+        double bmi = weightInKilograms / (heightInMeters * heightInMeters);
 
         System.out.printf("BMI is %.2f\n", bmi);
     }
